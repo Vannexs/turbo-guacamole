984ec5a23bd5583513494cdc2b1449ab60bc5568
Vannexs <vickyannexiss@gmail.com>
2025-03-04 06:19:36 +0300

    some comments added

--- type.py
+++ type.py
@@ -1,3 +1,13 @@
 a = fast awake and having coffee
 print(a)
-print("i have coffee now!")
+print("i have coffee now!") 
+# to this one:
+a = fast awake and having coffee
+print(a)
+print("i have coffee now!") 
+
+# both lines of code do the same thing, but they are written in two different ways.
+# first one is written in a way that is more readable and understandable.
+# second one is written in a way that is less readable and understandable.
+# the first one is written in a way that is more readable and understandable.
+# the second one is written in a way that is less readable and understandable.
